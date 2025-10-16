eb3e2601c6b5b217dd2b48260f9bfc3a63543764
N-Rock <nvschoenrock@gmail.com>
2025-10-16 13:32:34 -0500

    Add files via upload

---
+++ Data_Structures_Hello_World.java
@@ -0,0 +1,10 @@
+package data_structures_hello_world;
+
+public class Data_Structures_Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
